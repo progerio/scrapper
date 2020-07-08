90c8f2acc6e885f1f6b73445af261e753951439d
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2020-07-08 09:42:02 -0300

    feat: Persistindo no mongodb

--- src/app.js
+++ src/app.js
@@ -2,24 +2,50 @@ const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
 const { fetchLancamento } = require('./scrapper');
+const mongoose = require('mongoose');
+
+const stringConnection = 'mongodb://localhost:27017/test';
+
+mongoose.connect(stringConnection, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => { if (error) return console.log(error) });
+mongoose.Promise = global.Promise;
+
+const database = mongoose.connection;
+
+const LancamentoModel = require('./schema/lancamentoSchema.js');
 
 const app = async() => {
-        const directory = path.join(__dirname, '../html');
+    const directory = path.join(__dirname, '../html');
 	const files = fs.readdirSync(directory);
 	const browser = await puppeteer.launch();
-       for (let file of files) {
+        for (let file of files) {
 		const page = await browser.newPage();
 		if( path.extname(file) === '.html' ){
 			let pathFile = path.join('file://', __dirname, '../html', file);
 			await page.goto(pathFile);
 			const lancamento = await fetchLancamento(page);
-			console.log(lancamento);
+			let resultado = await LancamentoModel.findOne({identificador: lancamento.identificador});
+			
+			if(!resultado){
+				const document = new LancamentoModel(lancamento);
+				 document.save((err, doc) => {
+                   			 if (err) return console.error(err);
+                    			 console.log(lancamento.identificador);
+                		});
+				if (!fs.existsSync('./importados')) {
+    				    fs.mkdirSync('./importados');
+				}	
+				let pathAtual = path.join(__dirname, '../html', file);
+				let newPath = path.join(__dirname, '../importados', file);
+				
+				fs.rename(pathAtual, newPath, err => { if (err) return console.log(err); });
+			}
+			
 		}
 		await page.close();
        }
 	await browser.close();
-
+	database.close();
+	console.log('Pronto');
 }
 
 app();
-
---
+++ src/schema/lancamentoSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const LancamentoSchema = new Schema({
+	identificador: String,
+	empresa : {
+		razaoSocial: String,
+		cnpj: String,
+		endereco: {
+			rua: String,
+			numero: String,
+			complemento: String,
+			bairro: String,
+			cidade: String,
+			estado: String
+		}
+	},
+	data: String,
+	formaPagamento: String,
+	items:[
+		{
+			descricao: String,
+			codigo: Number,
+			quantidade: Number,
+			vaor: Number
+		}
+	],
+	total: Number,
+	dataCadastro: { type: Date, default: new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' }) }
+});
+
+module.exports = mongoose.model('Lancamento', LancamentoSchema);
+
+
+	
+
+
+
+
+
